72a282c273444b396473905f7a88e87aad0fb5bc
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-05-01 21:46:07 -0600

    hovers interesantes

--- JavaScript2Ex/js/scripts.js
+++ JavaScript2Ex/js/scripts.js
@@ -6,3 +6,24 @@ function verificar(control, max, progressText) {
         progressText.innerHTML = "Usted tiene un espacio de "+contador+" caracteres restantes.";
     }
 }
+
+customElements.define('mi-tarjeta',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadowRoot = this.attachShadow({mode: 'open'});
+
+      const style = document.createElement('style');
+      style.textContent = `
+        div { padding: 10px; border: 1px solid gray; width: 200px; margin: 10px; }
+        h2 { margin: 0 0 10px; }
+        ul { margin: 0; }
+        p { margin: 10px 0; }
+      `;
+
+      shadowRoot.appendChild(style);
+      shadowRoot.appendChild(templateContent.cloneNode(true));
+    }
+  }
+);
