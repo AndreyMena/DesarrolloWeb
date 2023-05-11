c08a4018a30e8ef7aef1b1f740a4cd59305a6132
Andrey Esteban Mena Espinoza <c04740@ecci.ucr.ac.cr>
2023-05-11 14:03:23 -0600

    vue project

---
+++ vueJS/todo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      To Do List
+    </h4>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+ <style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
