c0722e65b5f87aed8df632ed95783f0bcab0fa54
Andrey Esteban Mena Espinoza <c04740@ecci.ucr.ac.cr>
2023-05-22 14:00:01 -0600

    Done

--- visitasJS/prueba.js
+++ visitasJS/prueba.js
@@ -1,12 +1,6 @@
-
-
-//const { fstat, access } = require('fs');
 const fs = require('fs');
 const http = require('http');
 var url = require('url');
-//const { URL } = require('inspector');
-//const URL = require('url');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -18,51 +12,18 @@ function liste(res) {
 
     var counter = 0;
     for (const registro of registros) {
-        //counter++;
         const visita = registro.split(':');
         console.log(visita);
         if (visita.length === 1) {
 
         }else{
             console.log(visita.length)
-            //res.write(visita[2]);
-            //const comentario = visita[3].replace('<br />', '\n');
-    
-            //res.write('<p><em>Comentario enviado por <a href="mailto:${visita[1]}">${visita[0]}</a>.</em><br />Fecha: ${visita[2]}<br /><q>${comentario}</q></p>');
             res.write('<p><em>Comentario enviado por <a href="mailto:'+visita[1]+'">'+visita[0]+'</a>.</em><br />Fecha: '+visita[2]+'<br /><q>'+visita[3]+'</q></p>');
         }
     }
 
     fs.closeSync(archivo);
 }
-/*
-function liste(res)
-{
-	//archivo = fopen('visitas.txt', 'r');
-    const filePath = 'visitas.txt';
-
-    
-    fs.readFile(filePath, 'utf8', (error, data) => {
-        if (error) {
-            console.error('Error al leer el archivo:', error);
-            return;
-        }
-        res.write("aaaaaaaaaaaaaaaaa")
-      //visita = data.split(':');
-      //res.write( "<p><em>Comentario enviado por <a href=mailto:\"$visita[1]\">$visita[0]</a>.</em><br />Fecha: $visita[2]<br /><q>$visita[3]</q></p>");
-      //console.log('Contenido del archivo:', visita);
-    });
-
-    
-	while(registro = fs.readFile("visitas.txt")){
-		visita = explode(':', registro);
-		res.write( "<p><em>Comentario enviado por <a href=mailto:\"$visita[1]\">$visita[0]</a>.</em><br />Fecha: $visita[2]<br /><q>$visita[3]</q></p>");
-	} // while
-
-	fclose($archivo);
-    
-}
-*/
 
 function grabe(post) {
     const fecha = new Date();
@@ -79,14 +40,8 @@ function grabe(post) {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    //console.log(url.toString);
-    //const get = URL.parse(req.url, true).query;
     const get = url.parse(req.url, true).query;
 
-    //post = null;
-    //console.log(get.accion);
-    //var post;
-    //var post;
     if (req.method === 'POST') {
       let body = '';
       req.on('data', chunk => {
@@ -103,18 +58,15 @@ const server = http.createServer((req, res) => {
     }
     
     console.log(req.url);
-    //accion = get.accion || post.accion;
     switch(req.url) {
     case '/liste':
       res.write('<h1>Libro de Visitas</h1>');
       res.write(`<p><a href="${req.pathname || '/'}">Enviar un nuevo comentario.</a></p>`);
       liste(res);
       res.write(`<p><a href="${req.pathname || '/'}">Enviar un nuevo comentario.</a></p>`);
-      //console.log("hey");
       break;
     case '/grabe':
-        //console.log("uwu");
-        //console.log(post.nombre);
+
         const grabe = `
                 <p>
                     Comentario agregado exitosamente
@@ -122,11 +74,10 @@ const server = http.createServer((req, res) => {
 
             <a href="liste">Ver comentarios</a>
             <br />
-            
+            <br />
             <a href="liste">Agregar otro comentario</a>
             `;
         res.end(grabe);
-        //grabe(post);
       break;
     default:
 
@@ -155,8 +106,6 @@ const server = http.createServer((req, res) => {
     }
     res.end();
 });
-  
-
 
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${server.address().port}/`);
